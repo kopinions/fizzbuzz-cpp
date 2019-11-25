aa8a806a3d8ff27b20c5cfeca6a6b7f4a89acc12
sjkyspa <sjkyspa@gmail.com>
2019-11-25 23:22:11 +0800

    feat: add support for the second and third number support

--- tests/countoff.cpp
+++ tests/countoff.cpp
@@ -31,3 +31,35 @@ TEST(StudentTest, should_countoff_the_fizz_when_the_first_number_matched) {
 
     EXPECT_EQ("Fizz", ga->students().at(2)->countoff());
 }
+
+
+TEST(StudentTest, should_countoff_the_fizz_when_the_second_number_matched) {
+    std::vector<std::unique_ptr<student>> students;
+    for (auto i = 0; i < 100; i++) {
+        students.push_back(std::make_unique<student>(i + 1));
+    }
+//    const std::shared_ptr<game> &ga = std::make_shared<game>(game());
+
+    std::vector<rule> rules;
+    rules.emplace_back(5, "Buzz");
+    const std::shared_ptr<game> &ga = game::create(rules);
+    ga->involve(students);
+
+    EXPECT_EQ("Buzz", ga->students().at(4)->countoff());
+}
+
+
+TEST(StudentTest, should_countoff_the_fizz_when_the_third_number_matched) {
+    std::vector<std::unique_ptr<student>> students;
+    for (auto i = 0; i < 100; i++) {
+        students.push_back(std::make_unique<student>(i + 1));
+    }
+//    const std::shared_ptr<game> &ga = std::make_shared<game>(game());
+
+    std::vector<rule> rules;
+    rules.emplace_back(7, "Whizz");
+    const std::shared_ptr<game> &ga = game::create(rules);
+    ga->involve(students);
+
+    EXPECT_EQ("Whizz", ga->students().at(6)->countoff());
+}
