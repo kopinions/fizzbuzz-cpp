039a5d41b53927884a5c7af417e4354c03d2de69
sjkyspa <sjkyspa@gmail.com>
2019-11-25 21:15:35 +0800

    refactor: refactor with shared and unique ptr

--- include/game.hpp
+++ include/game.hpp
@@ -5,17 +5,31 @@
 #include <vector>
 #include "student.hpp"
 
-class game {
+class game: public std::enable_shared_from_this<game> {
 private:
-    std::vector<student> _students;
+    std::vector<std::unique_ptr<student>> _students;
 public:
-    game(std::vector<student> students) {
-        _students = students;
+    game() {
     }
-    std::vector<student> students() {
+
+    std::vector<std::unique_ptr<student>> &students() {
         return _students;
     }
+
+    std::shared_ptr<game> create() {
+        return shared_from_this();
+    }
+
+    void involve(std::vector <std::unique_ptr<student>>& students);
 };
+
+void game::involve(std::vector<std::unique_ptr<student>>& students) {
+    for (auto it = students.begin(); it < students.end(); it++) {
+        std::unique_ptr<student> &ptr = *it;
+
+        _students.push_back(std::make_unique<student>(**it));
+    }
+}
 
 
 #endif //FIZZBUZZ_GAME_HPP
--- tests/countoff.cpp
+++ tests/countoff.cpp
@@ -1,17 +1,18 @@
 #include <student.hpp>
 #include <game.hpp>
 #include <gtest/gtest.h>
+#include <memory>
 
 TEST(StudentTest, should_countoff_same_as_position_when_rule_is_empty) {
-    std::vector<student> students;
+    std::vector<std::unique_ptr<student>> students;
     for (auto i = 0; i < 100; i++) {
-        students.push_back(student(i + 1));
+        students.push_back(std::make_unique<student>(i + 1));
     }
-    game ga(students);
-    const std::vector<student> &stus = ga.students();
+    const std::shared_ptr<game> &ga = std::make_shared<game>(game());
+    ga->involve(students);
 
-    for (auto s : stus) {
-        student &student = s;
-        EXPECT_EQ(std::to_string(student.position()), student.countoff());
+    for (auto it = ga->students().begin(); it < ga->students().end(); it++) {
+        std::unique_ptr<student> &ptr = *it;
+        EXPECT_EQ(std::to_string(ptr->position()), ptr->countoff());
     }
 }
